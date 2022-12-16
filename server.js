deebf58a33e8a92f3d6d76e6d8f8c639bccad832
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-12-16 17:57:34 -0500

    run build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 
-
+const path = require('path');
 // server used to send emails
 const app = express();
 app.use(cors());
