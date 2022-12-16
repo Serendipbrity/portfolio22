505eb43d842bd0e131b6ae9aeacfd321b8b6c62a
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-12-15 21:59:34 -0500

    pdf icons and links added in nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Col, Row } from "react-bootstrap";
-import navIcon1 from "../assets/img/nav-icon1.svg";
+import linkdInIcon from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
-import navIcon4 from "../assets/img/nav-icon4.svg";
+import downloadIcon from '../assets/img/file-download-import-icon.svg';
+import githubIcon from "../assets/img/nav-icon4.svg";
 import logo from "../assets/img/logo.svg";
 
 export const Footer = () => {
@@ -16,13 +16,13 @@ export const Footer = () => {
           <Col sm={6} className="text-center text-sm-end">
             <div className="social-icon social-icons-footer-only">
               <a href="https://github.com/Serendipbrity">
-              <img src={navIcon4} />
+              <img src={githubIcon} />
               </a>
               <a href="https://www.linkedin.com/in/thanksfrom3rittani/">
-                <img src={navIcon1} />
+                <img src={linkdInIcon} />
               </a>
               <a href="">
-                <img src={navIcon3} />
+                <img src={downloadIcon} fill='white'/>
               </a>
             </div>
             <p>Copyright 2022. All rights reserved.</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import linkdInIcon from '../assets/img/nav-icon1.svg';
+import githubIcon from '../assets/img/nav-icon4.svg';
+import downloadIcon from '../assets/img/file-download-import-icon.svg';
 import me from "../assets/img/Me.jpeg"
 
 export const NavBar = () => {
@@ -54,9 +54,9 @@ export const NavBar = () => {
                     </Nav>
                     <span className='navbar-text'>
                         <div className='social-icon'>
-                            <a href='#'><img src={navIcon1} alt="" /></a>
-                            <a href='#'><img src={navIcon2} alt="" /></a>
-                            <a href='#'><img src={navIcon3} alt=""/></a>
+                            <a href='https://www.linkedin.com/in/thanksfrom3rittani/'><img src={linkdInIcon} alt="" /></a>
+                            <a href='https://github.com/Serendipbrity'><img src={githubIcon} alt="" /></a>
+                            <a href='#'><img src={downloadIcon} alt=""/></a>
                         </div>
                         <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                     </span>
